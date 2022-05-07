fe5cde519bf84b78bcd1df2ba6187644cbd9aa45
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-05-07 22:50:34 +0500

    finished

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 from black import out
 import cv2
 import numpy as np
+import playsound
+import smtplib
 
 
-video=cv2.VideoCapture('fire_video.mov')
+fire_reported = 0
+alarm_status = False
+
+
+def play_audio():
+    playsound.playsound('alarm.mp3', True)
+
+
+video=cv2.VideoCapture(1)
 
 while True:
     ret, frame = video.read()
-    frame =cv2.resize(frame, (1000, 600))
     blur =cv2.GaussianBlur(frame, (15,15), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
@@ -21,17 +30,22 @@ while True:
 
     output=cv2.bitwise_and(frame,hsv,mask=mask)
     
-    number_of_total = cv2.countNonZero(mask)
-
-    if int(number_of_total) > 15000:
-        print('fire detected')
-
+    size = cv2.countNonZero(mask)
 
     if ret == False:
         break
-    cv2. imshow("Output", output)
+    cv2.imshow("Output", output)
 
     if cv2. waitKey(1)& 0xFF == ord("q"):
         break
+
+    if int(size) > 15000:
+        print('Fire detected')
+        fire_reported = fire_reported + 1
+        if fire_reported >= 1:
+            if alarm_status == False:
+                play_audio()
+                alarm_status = True
+
 cv2. destroyAllWindows ()
 video.release()
